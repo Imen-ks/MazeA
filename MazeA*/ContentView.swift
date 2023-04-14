554afac0c3c485ec2046e9ff939e73a71bc5eb1d
Imen Ksouri <imen_ksouri@hotmail.fr>
2023-04-14 21:23:57 +0200

    Initial Commit

---
+++ MazeA*/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MazeA*
+//
+//  Created by Imen Ksouri on 14/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MazeA*/MazeA_App.swift
@@ -0,0 +1,17 @@
+//
+//  MazeA_App.swift
+//  MazeA*
+//
+//  Created by Imen Ksouri on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MazeA_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
